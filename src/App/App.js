438725452d3392b7d59a9449f6042cbac228c304
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2022-12-09 16:06:41 -0600

    add monaco editor

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,43 @@
 import './App.css';
+import Editor from "@monaco-editor/react";
+import { useRef } from 'react';
 
 export function App() {
+  const editorRef = useRef(null);
+  const monacoRef = useRef(null);
+
+  function editorMount(editor, monaco) {
+    editorRef.current = editor;
+    monacoRef.current = monaco;
+
+    monaco.languages.typescript.javascriptDefaults.addExtraLib([
+      'declare class Facts {',
+      '    /**',
+      '     * Returns the next fact',
+      '     */',
+      '    static next():string',
+      '}',
+    ].join('\n'), 'filename/facts.d.ts');
+  }
+
+
   return (
     <div className="App">
       <h1>My IDE</h1>
       <p>Filler text</p>
+      <Editor
+        height="90vh"
+        defaultLanguage="javascript"
+        theme='vs-dark'
+        options={{
+          fontSize: 22,
+          cursorSmoothCaretAnimation: true,
+          scrollBeyondLastLine: true,
+          smoothScrolling: true,
+        }}
+        onMount={editorMount}
+        defaultValue="// some comment"
+      />
     </div>
   );
 }
--- test.js
+++ test.js
@@ -1,6 +1,9 @@
+// import * as _ from "./lib/dist/index.js";
 /// <reference path="./lib/dist/index.d.ts" />
-import * as _ from "./lib/dist/index.js";
-
-console.log(_.grid('h', 2, 2));
-console.log([2, 3].max());
-console.log((69).chr());
+(async () => {
+    const _ = await import("./lib/dist/index.js");
+    
+    console.log(_.grid('h', 2, 2));
+    console.log([2, 3].max());
+    console.log((69).chr());
+})();
