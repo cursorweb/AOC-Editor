d68e557f35b9d1ed5f68c61ac5974ac085403783
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2022-12-09 17:58:21 -0600

    add counter class

--- lib/src/utils.ts
+++ lib/src/utils.ts
@@ -32,3 +32,61 @@ export function grid<T>(val: T, ...sizes: number[]): RecArray<T> {
 
     return out;
 }
+
+/**
+ * Automatically sets undefined values to the default value.
+ * For example,
+ * ```js
+ * const counter = new AutoMap(0);
+ * counter.incr("x");
+ * console.log(counter.map); // { x: 1 }
+ * ```
+ */
+export class Counter<K extends string, V extends number> {
+    /**
+     * The internal hashmap that is being edited
+     */
+    public map: Map<K, V>;
+    private v: V;
+
+    constructor(v: V = 0 as V) {
+        this.map = new Map();
+        this.v = v;
+    }
+
+    /**
+     * Increase the value of `key`. Should only be used with numbers.
+     * Equivalent to `.set(key, x => x + 1)`
+     * @param key The key to set
+     */
+    public incr(key: K) {
+        this.set(key, v => v as number + 1 as V);
+    }
+
+    /**
+     * Sets the key.
+     * @param key Key
+     * @param f Function to increase
+     */
+    public set(key: K, f: (v: V) => V) {
+        let val = this.map.get(key);
+
+        if (!val) {
+            this.map.set(key, this.v);
+            val = this.v;
+        }
+
+        val = f(val);
+
+        this.map.set(key, val);
+    }
+
+    /**
+     * Get's the key.
+     * @param key Key
+     * @returns Val
+     */
+    public get(key: K) {
+        return this.map.get(key) || this.v;
+    }
+}
